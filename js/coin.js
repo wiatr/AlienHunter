d3c28f1a11673dd5fac96bad0cd63ac7bdb1c598
Krzysztof Wiatr <krzywiatr@gmail.com>
2017-10-08 14:44:30 +0200

    remove

--- js/coin.js
+++
@@ -1,6 +0,0 @@
-var Coin = function(){
-    this.x = Math.floor(Math.random() * 10);
-    this.y = Math.floor(Math.random() * 10);
-}
-
-module.exports = Coin;
--- js/furry.js
+++
@@ -1,7 +0,0 @@
-var Furry = function(){
-    this.x = 0;
-    this.y = 0;
-    this.direction = "right";
-}
-
-module.exports = Furry;
